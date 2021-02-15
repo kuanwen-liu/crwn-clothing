6710765df14ad587ef48b63d066213dd1c0e49b6
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2021-02-14 21:27:15 -0500

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
@@ -32,8 +32,6 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
-
-          console.log(this.state);
         });
       } else {
         this.setState({ currentUser: userAuth });
